ffbeba5614a2e7bff99fa8075c9b72db801fc99d
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-05-13 23:54:49 +0800

    setup

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -3,7 +3,9 @@
 export default function LoginPage() {
     return (
         <>
-            <h1>Login setup</h1>
+            <div className="flex items-center justify-center min-h-screen">
+                <h1 className="text-8xl">Login setup</h1>
+            </div>
         </>
     );
 }
