19470ca4cab973ccc4b9f30d4a9abd04fbd1af7d
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-05-15 21:35:44 +0800

    add everyrole

--- client/src/components/LandingPage/EveryRole/EveryRole.tsx
+++ client/src/components/LandingPage/EveryRole/EveryRole.tsx
@@ -4,42 +4,93 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 type EveryRoleProps = {
     setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
-}
+};
 
 gsap.registerPlugin(ScrollTrigger);
 
-export default function EveryRole({ setIsDarkMode }: EveryRoleProps) {
+export default function EveryRole({
+    setIsDarkMode,
+}: EveryRoleProps) {
     const sectionRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        gsap.to("body", {
-            backgroundColor: "#000000",
-            color: "#ffffff",
+        const trigger = ScrollTrigger.create({
+            trigger: sectionRef.current,
 
-            scrollTrigger: {
-                trigger: sectionRef.current,
-                start: "top center",
-                markers: true,
-                toggleActions: "play reverse play reverse",
-                onEnter: () => {
-                    setIsDarkMode(true);
-                },
+            start: "top 70%",
+            end: "bottom 30%",
 
-                onLeaveBack: () => {
-                    setIsDarkMode(false);
-                }
+            markers: true,
+
+            invalidateOnRefresh: true,
+
+            onEnter: () => {
+                setIsDarkMode(true);
+            },
+
+            onEnterBack: () => {
+                setIsDarkMode(true);
+            },
+
+            onLeave: () => {
+                setIsDarkMode(false);
+            },
+
+            onLeaveBack: () => {
+                setIsDarkMode(false);
             },
         });
 
+        ScrollTrigger.refresh();
+
         return () => {
-            ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+            trigger.kill();
         };
-    }, []);
+    }, [setIsDarkMode]);
 
     return (
-        <div ref={sectionRef}
-            className="flex items-center justify-center min-h-screen">
-            <h1 className="font-secondary text-center text-[100px]">LABORATORY <br />OPERATIONS SHOULD <br /> FEEL SEAMLESS, FAST AND INTELLIGENT.</h1>
+        <div
+            ref={sectionRef}
+            className="relative flex items-center justify-center py-20"
+        >
+            <h1
+                className="font-secondary font-light text-center text-[clamp(40px,12.82px+7.55vw,90px)]
+                lg:text-[clamp(75px,calc(75px+(140-75)*((100vw-1024px)/(1920-1024))),140px)]"
+            >
+                LABORATORY <br />
+                OPERATIONS <br className="lg:hidden" />
+                SHOULD <br />
+                FEEL{" "}
+                <span className="relative inline-block">
+                    <span className="relative z-10">
+                        SEAMLESS
+                    </span>
+
+                    <svg
+                        className="absolute left-1/2 top-1/2 w-[115%] h-[160%] -translate-x-1/2 -translate-y-1/2"
+                        viewBox="0 0 100 40"
+                        preserveAspectRatio="none"
+                    >
+                        <ellipse
+                            cx="50"
+                            cy="20"
+                            rx="44"
+                            ry="13"
+                            fill="none"
+                            stroke="white"
+                            strokeWidth="0.2"
+                        />
+                    </svg>
+                </span>
+                , <br className="lg:hidden" />
+                <span className="font-primary font-semibold text-[#bf3419]">
+                    FAST
+                </span>{" "}
+                AND <br />
+                <span className="font-primary font-semibold text-[#f4aa29]">
+                    INTELLIGENT
+                </span>
+            </h1>
         </div>
     );
 }
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
+
 import NavBar from "../components/LandingPage/NavBar/NavBar";
 import HeroSection from "@/components/LandingPage/HeroSection/HeroSection";
-import GenerateReports from "@/components/LandingPage/GenerateReports/GenerateReports"
-import HowItWorks from "@/components/LandingPage/HowItWorks/HowItWorks"
-import ResolveEfficiently from "@/components/LandingPage/ResolveEfficiently/ResolveEfficiently"
-import SubmitRequest from "@/components/LandingPage/SubmitRequest/SubmitRequest"
-import TrackAndManage from "@/components/LandingPage/TrackAndManage/TrackAndManage"
+import GenerateReports from "@/components/LandingPage/GenerateReports/GenerateReports";
+import HowItWorks from "@/components/LandingPage/HowItWorks/HowItWorks";
+import ResolveEfficiently from "@/components/LandingPage/ResolveEfficiently/ResolveEfficiently";
+import SubmitRequest from "@/components/LandingPage/SubmitRequest/SubmitRequest";
+import TrackAndManage from "@/components/LandingPage/TrackAndManage/TrackAndManage";
 import EveryRole from "@/components/LandingPage/EveryRole/EveryRole";
 
 export default function LandingPage() {
-
     const [isDarkMode, setIsDarkMode] = useState(false);
 
     useEffect(() => {
@@ -17,21 +17,31 @@ export default function LandingPage() {
     }, []);
 
     return (
-        <>
-            <div className="flex flex-col">
-                <NavBar />
-                <div className="mb-30">
-                    <HeroSection />
-                </div>
+        <main
+            className={`
+                flex flex-col
+                transition-colors duration-500
+                ${
+                    isDarkMode
+                        ? "bg-black text-white"
+                        : "bg-white text-black"
+                }
+            `}
+        >
+            <NavBar />
 
-                <HowItWorks />
-                <SubmitRequest />
-                <TrackAndManage />
-                <ResolveEfficiently />
-                <GenerateReports isDarkMode={isDarkMode} />
-                <EveryRole setIsDarkMode={setIsDarkMode} />
-                <ResolveEfficiently />
+            <div className="mb-30">
+                <HeroSection />
             </div>
-        </>
+
+            <HowItWorks />
+            <SubmitRequest />
+            <TrackAndManage />
+            <ResolveEfficiently />
+
+            <GenerateReports isDarkMode={isDarkMode} />
+
+            <EveryRole setIsDarkMode={setIsDarkMode} />
+        </main>
     );
 }
