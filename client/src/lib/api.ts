fe67e0ff814121da47c517092385df54ef097fe6
John Patrick Soriaga <patricksoriaga14@gmail.com>
2026-05-19 21:48:10 +0800

    add refresh

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -14,21 +14,34 @@ export const privateFetch = async (url: string, options: RequestInit = {}) => {
 
     let res = await makeRequest();
 
-    {/*if (res.status === 401) {
-        localStorage.removeItem("accessToken");
-        window.location.href = "/";
-        throw new Error("Session expired");
-    }*/}
+    if (res.status === 401) {
+        const refreshRes = await fetch("http://localhost:3000/api/auth/refresh", {
+            method: "POST",
+            credentials: "include",
+        });
+
+        if (!refreshRes.ok) {
+            localStorage.removeItem("accessToken");
+            window.location.href = "/";
+            throw new Error("Session expired");
+        }
+
+        const data = await refreshRes.json();
+        accessToken = data.accessToken;
+        localStorage.setItem("accessToken", accessToken);
+
+        res = await makeRequest();
+    }
 
     return res;
 };
 
 export const publicFetch = (url: string, options: RequestInit = {}) => {
-  return fetch(url, {
-    ...options,
-    headers: {
-      "Content-Type": "application/json",
-      ...(options.headers || {}),
-    },
-  });
+    return fetch(url, {
+        ...options,
+        headers: {
+            "Content-Type": "application/json",
+            ...(options.headers || {}),
+        },
+    });
 };
