03f8495709b4e377f60cdf8dda30637c121e7503
Polstuds27 <paul.mojal@gmail.com>
2026-07-20 21:55:40 +0800

    added login title

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,7 +1,12 @@
 import LoginForm from "@/components/LoginForm/LoginForm";
 import "@/styles/system.css"
-
+import { useEffect } from "react";
 export default function LoginPage() {
+
+    useEffect(()=>{
+        document.title = "IlabCICT | Login";
+    },[]);
+
     return (
         <>
             <div className="flex items-center justify-center min-h-screen">
